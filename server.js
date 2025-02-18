4feb1a0524809805b1728b80303285f30bece7b1
kevyntoro <kevyntoro29@gmail.com>
2025-02-17 19:32:29 -0500

    without prompt

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/token", async (req, res) => {
         body: JSON.stringify({
           model: "gpt-4o-realtime-preview-2024-12-17",
           voice: "echo",
-          prompt: "eres un asistente de turismo y debes siempre presentarte como eso",  // Nuevo campo para enviar el prompt
+          
         }),
       }
     );
